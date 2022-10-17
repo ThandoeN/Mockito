c57f677933c157fede80c72566b25358680d12cc
Thando Ngwabi <thando10ngwabi@gmail.com>
2022-10-17 16:47:37 +0200

    test

---
+++ src/test/java/com/mockito/mock/PatientRecordControllerTest.java
@@ -0,0 +1,68 @@
+package com.mockito.mock;
+
+import com.mockito.mock.controller.PatientRecordController;
+import com.mockito.mock.entity.PatientRecord;
+import com.mockito.mock.repository.PatientRecordRepository;
+import io.restassured.mapper.ObjectMapper;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PatientRecordController.class)
+public class PatientRecordControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @MockBean
+    PatientRecordRepository patientRecordRepository;
+
+    PatientRecord RECORD_1 = new PatientRecord(1l, "Rayven Yor", 23, "Cebu Philippines");
+    PatientRecord RECORD_2 = new PatientRecord(2l, "David Landup", 27, "New York USA");
+    PatientRecord RECORD_3 = new PatientRecord(3l, "Jane Doe", 31, "New York USA");
+
+    @Test
+    public void getAllRecords_success() throws Exception {
+        List<PatientRecord> records = new ArrayList<>(Arrays.asList(RECORD_1, RECORD_2, RECORD_3));
+
+        Mockito.when(patientRecordRepository.findAll()).thenReturn(records);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/patient")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) jsonPath("$", Matchers.hasSize(3)))
+                .andExpect((ResultMatcher) jsonPath("$[2].name", Matchers.is("Jane Doe")));
+    }
+
+    @Test
+    public void getPatientById_success() throws Exception {
+        Mockito.when(patientRecordRepository.findById(RECORD_1.getPatientId())).thenReturn(java.util.Optional.of(RECORD_1));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/patient/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) jsonPath("$", Matchers.notNullValue()))
+                .andExpect((ResultMatcher) jsonPath("$.name", Matchers.is("Rayven Yor")));
+    }
+
+
+}
